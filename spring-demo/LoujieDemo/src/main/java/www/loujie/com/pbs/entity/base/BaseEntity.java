da70f660d9d190e0434042791a388af9544d8f03
loujie <805240128@qq.com>
2016-12-30 10:30:09 +0800

    spring测试用例

---
+++ spring-demo/LoujieDemo/src/main/java/www/loujie/com/pbs/entity/base/BaseEntity.java
@@ -0,0 +1,15 @@
+package www.loujie.com.pbs.entity.base;
+
+import java.io.Serializable;
+
+/**
+ * 基础实体
+ * 
+ * @author loujie
+ *
+ */
+public class BaseEntity implements Serializable {
+
+	private static final long serialVersionUID = 8978379289883046391L;
+
+}
