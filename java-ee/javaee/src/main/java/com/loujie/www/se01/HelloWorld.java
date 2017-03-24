18a09ef171401df2eb320fa71eeb626d1555e849
loujie <805240128@qq.com>
2017-03-24 14:30:34 +0800

    提交到网络

---
+++ java-ee/javaee/src/main/java/com/loujie/www/se01/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.loujie.www.se01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.err.println("Hello world!");
+	}
+
+	class World {
+
+	}
+
+}
+
+class Hello {
+
+	class World {
+
+	}
+}
