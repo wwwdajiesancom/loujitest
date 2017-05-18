c813f264d92563d682a9944ab2ee6b4bfcda3c92
loujie <805240128@qq.com>
2017-05-18 14:28:55 +0800

    ox

---
+++ java-ee/javaee/src/main/java/com/loujie/www/lucene3_5_0/LuceneUtils.java
@@ -0,0 +1,11 @@
+package com.loujie.www.lucene3_5_0;
+
+/**
+ * Lucene的基础操作
+ * 
+ * @author loujie
+ *
+ */
+public class LuceneUtils {
+
+}
---
+++ java-ee/javaee/src/test/java/com/loujie/www/lucene3_5_0/LuceneHelloWord.java
@@ -0,0 +1,17 @@
+package com.loujie.www.lucene3_5_0;
+
+import org.junit.Test;
+
+public class LuceneHelloWord {
+
+	/**
+	 * 1.测试创建索引
+	 */
+	@Test
+	public void createIndex1() {
+		
+		
+		
+	}
+
+}
